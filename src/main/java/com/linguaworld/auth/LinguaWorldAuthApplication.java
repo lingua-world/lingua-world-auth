4ea0a85562060f534457b39b86180ef4be414499
Anton Rynkovoy <anton.rynkovoy@gmail.com>
2018-07-05 22:52:53 +0300

    Initial commit

---
+++ src/main/java/com/linguaworld/auth/LinguaWorldAuthApplication.java
@@ -0,0 +1,12 @@
+package com.linguaworld.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguaWorldAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinguaWorldAuthApplication.class, args);
+	}
+}
---
+++ src/test/java/com/linguaworld/auth/LinguaWorldAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.linguaworld.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LinguaWorldAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
